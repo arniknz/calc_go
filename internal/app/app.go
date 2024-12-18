d10187970f913293ebdf702823471a5c172c6ce5
arn <arniknz@gmail.com>
2024-12-18 22:54:07 +0300

    Update

--- internal/app/app.go
+++ internal/app/app.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Request struct {
-	Expression string `json:"кулькулятор бебебе"`
+	Expression string `json:"expression"`
 }
 
 func CalcHandler(w http.ResponseWriter, r *http.Request) {
--- pkg/calculator/errors.go
+++ pkg/calculator/errors.go
@@ -3,8 +3,8 @@ package calculator
 import "errors"
 
 var (
-	ErrInvalidExpression     = errors.New(`json:"error: invalid expression"`)
-	ErrDivisionByZero        = errors.New(`json:"error: division by zero"`)
-	ErrMismatchedParentheses = errors.New(`json:"error: mismatched parentheses"`)
-	ErrInvalidCharacter      = errors.New(`json:"error: invalid character"`)
+	ErrInvalidExpression     = errors.New(`"error: invalid expression"`)
+	ErrDivisionByZero        = errors.New(`"error: division by zero"`)
+	ErrMismatchedParentheses = errors.New(`"error: mismatched parentheses"`)
+	ErrInvalidCharacter      = errors.New(`"error: invalid character"`)
 )
