a0903d715dbcd5c6c27caddda88ca0c71e4df8ae
arn <arniknz@gmail.com>
2024-12-18 21:03:07 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,12 @@
+package main
 
+import (
+	application "github.com/arniknz/calc_go/internal/app"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/v1/calculate", application.HandleRequest).Methods("POST")
+
+	application.StartServer()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/arniknz/calc_go
 
 go 1.23.4
+
+require github.com/gorilla/mux v1.8.1
