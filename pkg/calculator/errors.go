3d43f354f185e0146ba469284527d9beca84598d
arn <arniknz@gmail.com>
2024-12-19 15:41:03 +0300

    Update errors.go

--- pkg/calculator/errors.go
+++ pkg/calculator/errors.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	ErrInvalidExpression     = errors.New(`{"error": "invalid expression"}`)
 	ErrDivisionByZero        = errors.New(`{"error": "division by zero"}`)
-	ErrMismatchedParentheses = errors.New(`{"error": mismatched parentheses"}`)
+	ErrMismatchedParentheses = errors.New(`{"error": "mismatched parentheses"}`)
 	ErrInvalidCharacter      = errors.New(`{""error": "invalid character"}`)
 )
